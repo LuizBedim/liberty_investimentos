610dd9e1e7d2a2ace9ef20c9e50faf623347efab
LuizBedim <LuizOMBedim@gmail.com>
2022-10-24 16:17:10 -0300

    Modificações no Sobre Nós

---
+++ php/sobreNos.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- style.css -->
+    <link rel="stylesheet" type="text/css" href="../sobre-nos/main.css">
+    <!-- Icone da página -->
+    <link rel="shortcut icon" href="../img/icon2.ico">
+    <!-- Font-family KoHo -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo:wght@200&display=swap" rel="stylesheet">
+    <!-- Bootstrap --> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>Liberty</title>
+</head>
+<body>
+
+    <div class="container-main">
+        <div class="config-main">
+                <div class="left-side">
+                    <h1>Liberty</h1>
+                </div>
+
+                <div class="right-side">
+                    <div class="text-sobre-nos">
+                        <p>Sobre Nós<br>Lorem Ipsum is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                            unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
+                            not only five centuries.</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+        </script>
+</body>
+</html>
