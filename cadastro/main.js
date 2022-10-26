5c028635dd7da316c69485224f818f6dc6dca2ab
LuizBedim <LuizOMBedim@gmail.com>
2022-10-26 19:44:39 -0300

    cadastro

--- cadastro/main.js
+++ cadastro/main.js
@@ -1,4 +1,4 @@
-const input = document.querySelector('cpf')
+const input = document.querySelector('cpf', 'celular', 'cep')
 
 cpf.addEventListener('keypress', () => {
     let inputlenght = cpf.value.length
@@ -7,5 +7,25 @@ cpf.addEventListener('keypress', () => {
         cpf.value += '.'
     } else if (inputlenght === 11) {
         cpf.value += '-'
+    }
+})
+
+celular.addEventListener('keypress', () => {
+    let celularlenght = celular.value.length
+
+    if (celularlenght === 0) {
+        celular.value += '('
+    } else if (celularlenght === 3) {
+        celular.value += ')'
+    } else if (celularlenght === 9) {
+        celular.value += '-'
+    } 
+})
+
+cep.addEventListener('keypress', () => {
+    let ceplenght = cep.value.length
+
+    if (ceplenght === 5) {
+        cep.value += '-'
     }
 })
