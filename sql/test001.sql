76adc4c3165508b160230a06340a7e16ba666021
LuizBedim <LuizOMBedim@gmail.com>
2022-11-20 18:37:58 -0300

    Área restrita finalizada

--- sql/test001.sql
+++ sql/test001.sql
@@ -6,4 +6,4 @@ SELECT * FROM super_usuarios;
 
 INSERT INTO super_usuarios (usuario, senha, codigo) VALUES ('Luiz', '123', '321');
 
-INSERT INTO clientes (nome, sobrenome, email, senha, cpf, celular, cep, numero) VALUES ("Luiz", "Bedim", "teste2@gmail.com", "123", "999.999.999-00", "(11)99714-1247", "19013-240", "1000");
+INSERT INTO clientes (nome, sobrenome, email, senha, cpf, celular, cep, numero) VALUES ("Luiz", "Bedim", "teste4@gmail.com", "123", "999.999.999-00", "(11)99714-1247", "19013-240", "1000");
